tidev/titanium-sdk

TIMOB-5236 Improved error message when Resources$NotFoundException occurs while trying to load drawable.

android/titanium/src/org/appcelerator/titanium/view/TiDrawableReference.java
@@ -536,7 +536,12 @@ public class TiDrawableReference
 			stream = blob.getInputStream();
 			
 		} else if (isTypeResourceId() && resourceId != UNKNOWN && context != null) {
-			stream = context.getTiApp().getResources().openRawResource(resourceId);
+			try {
+				stream = context.getTiApp().getResources().openRawResource(resourceId);
+			} catch (Resources.NotFoundException e) {
+				Log.e(LCAT, "Drawable resource could not be opened. Are you sure you have the resource for the current device configuration (orientation, screen size, etc.)?");
+				throw e;
+			}
 		}
 
 		return stream;

burakozturk16/titanium_mobile

Conflicts:
	android/titanium/src/java/org/appcelerator/titanium/view/TiDrawableReference.java

android/titanium/src/java/org/appcelerator/titanium/view/TiDrawableReference.java
@@ -618,9 +618,13 @@ public class TiDrawableReference
 
 		} else if (isTypeBlob() && blob != null) {
 			stream = blob.getInputStream();
-			
 		} else if (isTypeResourceId() && resourceId != UNKNOWN) {
-			stream = TiApplication.getInstance().getResources().openRawResource(resourceId);
+			try {
+				stream = TiApplication.getInstance().getResources().openRawResource(resourceId);
+			} catch (Resources.NotFoundException e) {
+				Log.e(LCAT, "Drawable resource could not be opened. Are you sure you have the resource for the current device configuration (orientation, screen size, etc.)?");
+				throw e;
+			}
 		}
 
 		return stream;